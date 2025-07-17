36776ba22b7e11e433862527edbc10b233f50ada
Muskan kumari <muskan252605@gmail.com>
2025-07-17 11:27:10 +0530

    Add files via upload

---
+++ Type_of_variable.py
@@ -0,0 +1,13 @@
+# Different types of variables
+a = 10            # integer
+b = 3.14          # float
+c = "Hello"       # string
+d = True          # boolean
+e = [1, 2, 3]     # list
+
+# Print the type of each variable
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+print("Type of c:", type(c))
+print("Type of d:", type(d))
+print("Type of e:", type(e))
