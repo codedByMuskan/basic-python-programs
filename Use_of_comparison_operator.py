c00547b8527b6536bc366707f59e9925e99383c9
Muskan kumari <muskan252605@gmail.com>
2025-07-17 11:56:07 +0530

    Add files via upload

---
+++ Use_of_comparison_operator.py
@@ -0,0 +1,9 @@
+# Get values from the user
+a = float(input("Enter value for a: "))
+b = float(input("Enter value for b: "))
+
+# Use comparison operator to check if a is greater than b
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
