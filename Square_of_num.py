ba6e9a626f8f97f6644c3a40c7ff96d078de4a6d
Muskan kumari <muskan252605@gmail.com>
2025-07-17 11:43:37 +0530

    Add files via upload

---
+++ Square_of_num.py
@@ -0,0 +1,8 @@
+# Ask the user to enter a number
+num = float(input("Enter a number: "))
+
+# Calculate the square
+square = num * num
+
+# Print the result
+print("The square of", num, "is", square)
