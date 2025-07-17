9cef44879363fe7e935fb7fdbec2e7b651455901
Muskan kumari <muskan252605@gmail.com>
2025-07-17 11:45:55 +0530

    Add files via upload

---
+++ Cube_of_num.py
@@ -0,0 +1,8 @@
+## Ask the user to enter a number
+num = float(input("Enter a number: "))
+
+# Calculate the cube
+cube = num * num * num
+
+# Print the result
+print("The cube of", num, "is", cube)
