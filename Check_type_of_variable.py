211e1c1b55f336ad4cd0a3a002d40eee048f4a22
Muskan kumari <muskan252605@gmail.com>
2025-07-17 11:53:34 +0530

    Add files via upload

---
+++ Check_type_of_variable.py
@@ -0,0 +1,8 @@
+# Ask the user to enter something
+value = input("Enter something: ")
+
+# Show what was entered
+print("You entered:", value)
+
+# Show the type of the value
+print("The type of the variable is:", type(value))
