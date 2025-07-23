6af91e607ebb0228dc91c4e6e32679fd744e4e10
Muskan kumari <muskan252605@gmail.com>
2025-07-23 15:35:35 +0530

    Add files via upload

---
+++ truthtable.py
@@ -0,0 +1,14 @@
+# truth table of "or"
+
+print("True or False is", True or False)
+print("True or True is", True or False)
+print("False or False is", True or False)
+print("False or True is", True or False)
+
+# truth table of " and "
+
+
+print("True and False is", True or False)
+print("True and  True is", True or False)
+print("False and False is", True or False)
+print("False and True is",True or False)
