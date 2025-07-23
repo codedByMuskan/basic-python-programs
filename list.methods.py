77271eec72344af69d9c8e51392693e2ca15e1a6
Muskan kumari <muskan252605@gmail.com>
2025-07-23 17:02:46 +0530

    Add files via upload

---
+++ list.methods.py
@@ -0,0 +1,16 @@
+friends = ["Muskan", "apple","youtube","instagram","68t3qy","647"]
+
+friends.append("Strong")
+print(friends)
+
+numbers = [23,34,76,42,45,1]
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+numbers.insert(3,8)
+print(numbers)
+numbers.pop(5)
+print(numbers)
+numbers.remove(1)
+print(numbers)
