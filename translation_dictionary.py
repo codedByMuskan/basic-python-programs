85faeda66b70b4d717c41f2f2c30acb4193ce23e
Muskan kumari <muskan252605@gmail.com>
2025-07-24 20:31:15 +0530

    Add files via upload

---
+++ translation_dictionary.py
@@ -0,0 +1,11 @@
+words = {
+    "madad":"help",
+    "chillana":"scream",
+    "haath":"hand",
+    "kalam":"pen",
+    "aalsi":"lethargic",
+}
+
+word = input("Enter the word you want meaning of:")
+
+print(words[word])
