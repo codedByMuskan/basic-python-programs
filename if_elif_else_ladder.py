fadd11b1d787ba595e17a1fc878a04ca48e7dd2a
Muskan kumari <muskan252605@gmail.com>
2025-07-24 21:20:26 +0530

    Add files via upload

---
+++ if_elif_else_ladder.py
@@ -0,0 +1,15 @@
+a = int(input("Enter your age:"))
+
+if(a>=18):
+     print("you are eligible to vote")
+     print("congratulations")
+     
+elif(a<0):
+    print("you are entering an invalid negative age")
+    
+elif(a==0):
+    print("you are entering 0 which is not a valid age")
+else:
+     print("you are mot eligible to vote")
+     
+print("end of program")
