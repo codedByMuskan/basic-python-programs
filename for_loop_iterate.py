3622a2b8b98b38b71a92227d9c6452b942d9ed98
Muskan kumari <muskan252605@gmail.com>
2025-07-25 16:29:59 +0530

    Add files via upload

---
+++ for_loop_iterate.py
@@ -0,0 +1,14 @@
+# for loop with lists
+l = [1,2,3,5,76,456,356]
+for i in l:
+    print(i)
+    
+# for loop with tuples
+t = (5,87,34,75)
+for i in t:
+    print(i)
+    
+#for loop with strings
+s = "Harry"
+for i in s:
+    print(i)
